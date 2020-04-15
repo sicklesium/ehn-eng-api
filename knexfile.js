bf8d69b163857e423e8923318d7613c3847b5fe5
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-04-15 00:46:22 -0400

    Minor adjustments

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
-  }
+  },
 
   // staging: {
   //   client: 'postgresql',
@@ -32,20 +32,18 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "dbmigrations",
+    },
+    seeds: { directory: "./data/seeds" },
+  }
 
 };
